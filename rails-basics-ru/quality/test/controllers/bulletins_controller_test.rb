34a3451c44efffba6bbfc9d4e5a7ba0e03908c94
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2024-11-22 08:09:44 +0300

    f

--- rails-basics-ru/quality/test/controllers/bulletins_controller_test.rb
+++ rails-basics-ru/quality/test/controllers/bulletins_controller_test.rb
@@ -4,19 +4,19 @@ require 'test_helper'
 
 class BulletinsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @bulletin = bulletins(:one) # Убедитесь, что в fixtures есть `bulletins.yml`
+    @bulletin = bulletins(:one)
   end
 
   test 'should get index' do
     get bulletins_url
     assert_response :success
-    assert_select 'h1', 'Bulletins' # Предполагается, что есть заголовок на странице
-    assert_select 'li', @bulletin.title # Проверьте отображение данных из fixtures
+    assert_select 'h1', 'Bulletins'
+    assert_select 'li', @bulletin.title
   end
 
   test 'should show bulletin' do
     get bulletin_url(@bulletin.id)
     assert_response :success
-    assert_select 'h1', @bulletin.title # Убедитесь, что заголовок совпадает
+    assert_select 'h1', @bulletin.title
   end
 end

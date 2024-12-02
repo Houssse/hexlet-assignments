108214ddf53109bdf51fdf52fb56cf9dab28eaa1
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2024-12-02 16:40:52 +0300

    end dz

--- rails-basics-ru/i18n/config/routes.rb
+++ rails-basics-ru/i18n/config/routes.rb
@@ -2,8 +2,12 @@
 
 Rails.application.routes.draw do
   # BEGIN
-  
-  # END
+  scope '(:locale)', locale: /en|ru/ do
+    root 'home#index'
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    resources :posts do
+      resources :comments
+    end
+  end
+  # END
 end

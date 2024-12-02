1eb934fb7382fc17055b9bcc4452672d61aeed95
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2024-12-02 18:16:57 +0300

    end dz

--- rails-basics-ru/middlewares/app/middlewares/set_locale_middleware.rb
+++ rails-basics-ru/middlewares/app/middlewares/set_locale_middleware.rb
@@ -6,6 +6,13 @@ class SetLocaleMiddleware
   end
 
   # BEGIN
-  
+  def call(env)
+    request = Rack::Request.new(env)
+    locale = request.env['HTTP_ACCEPT_LANGUAGE']&.scan(/^[a-z]{2}/)&.first
+
+    I18n.locale = I18n.available_locales.include?(locale&.to_sym) ? locale : I18n.default_locale
+
+    @app.call(env)
+  end
   # END
 end

16210cff251beeaa70adb759cd8e0b941c1555b5
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2024-12-31 08:04:38 +0300

    end dz

--- rails-real-ru/nested-forms/app/models/resume.rb
+++ rails-real-ru/nested-forms/app/models/resume.rb
@@ -5,6 +5,10 @@ class Resume < ApplicationRecord
   validates :contact, presence: true
 
   # BEGIN
-  
+  has_many :educations, class_name: 'ResumeEducation', dependent: :destroy
+  has_many :works, class_name: 'ResumeWork', dependent: :destroy
+
+  accepts_nested_attributes_for :educations, update_only: true, allow_destroy: true
+  accepts_nested_attributes_for :works, update_only: true, allow_destroy: true
   # END
 end

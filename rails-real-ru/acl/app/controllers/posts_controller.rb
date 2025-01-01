6e6609cf49441d060f96ecb363e744fd63d4d8e1
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2025-01-01 20:53:54 +0300

    end dz

--- rails-real-ru/acl/app/controllers/posts_controller.rb
+++ rails-real-ru/acl/app/controllers/posts_controller.rb
@@ -4,6 +4,65 @@ class PostsController < ApplicationController
   after_action :verify_authorized, except: %i[index show]
 
   # BEGIN
-  
+  include Pundit
+
+  before_action :set_post, only: %i[show edit update destroy]
+  before_action :authorize_post, only: %i[edit update destroy]
+
+  def index
+    @posts = Post.all
+    authorize @posts
+  end
+
+  def show
+    authorize @post
+  end
+
+  def new
+    @post = Post.new
+    authorize @post
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+    authorize @post
+
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_url, notice: 'Post was successfully deleted.'
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def authorize_post
+    authorize @post
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :category_id)
+  end
   # END
 end
--- rails-real-ru/acl/app/policies/post_policy.rb
+++ rails-real-ru/acl/app/policies/post_policy.rb
@@ -2,6 +2,31 @@
 
 class PostPolicy < ApplicationPolicy
   # BEGIN
-  
+  attr_reader :user, :post
+
+  def initialize(user, post)
+    @user = user
+    @post = post
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    user.present?
+  end
+
+  def update?
+    user.present? && (post.author == user || user.admin?)
+  end
+
+  def destroy?
+    user.present? && user.admin?
+  end
   # END
 end
